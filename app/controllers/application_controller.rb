e84c54f7760d8bd9b2f467b4db70124b9ef15582
miriam <miriam5000@gmail.com>
2017-12-02 16:50:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  configure do 	
+		enable :sessions
+		set :session_secret, "my_secret"
+	end 
+
+  	get '/' do
+    	erb :index
+ 	end
 end
