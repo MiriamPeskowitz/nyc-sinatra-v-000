2a8bcea67cf294c57958ea456f3e2156f92131ff
miriam <miriam5000@gmail.com>
2017-12-02 21:52:05 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -31,3 +31,26 @@ end
 # 		@song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
 # 		@song.genre_ids = params[:genres]
 # 		@song.save
+
+ # allows you to view form to create a new figure (FAILED - 1)
+ 		get figures/new
+
+ #  allows you to create a new figure with a title (FAILED - 2)
+ 		post figures/new complex 
+ #  allows you to create a new figure with a landmark (FAILED - 3)
+ 		post figures/new complex
+ #  allows you to create a new figure with a new title (FAILED - 4)
+ 		post figures/new complex
+ #  allows you to create a new figure with a new landmark (FAILED - 5)
+ 		post figures/new complex
+
+ 		also:   creates checkboxes for all the landmarks and titles created on the Figures new page (FAILED - 16)
+
+ #  allows you to list all figures (FAILED - 6)
+ 		get figures/index or show 
+ #  allows you to see a single Figure (FAILED - 7)
+ 		get figures/show/:id
+ #  allows you to view form to edit a single figure (FAILED - 8)
+ 		get figures/edit 
+ #  allows you to edit a single figure (FAILED - 9)
+ 		patch figures/edit 
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,21 @@
 class LandmarksController < ApplicationController
 
 end
+
+get landmark/new
+ allows you to view form to create a new landmark (FAILED - 10)
+ 		get landmark/new
+  allows you to create a new landmark (FAILED - 11)
+  		post landmark/new
+  allows you to list all landmarks (FAILED - 12)
+  	get landmark/index or get landmark/show 
+
+  allows you to see a single landmark (FAILED - 13)
+  	get landmark/new id? 
+
+  allows you to view the form to edit a single landmark (FAILED - 14)
+  		get landmark/edit
+  allows you to edit a single landmark (FAILED - 15)
+  	patch landmark/edit
+  creates checkboxes for all the landmarks and titles created on the Figures new page (FAILED - 16)
+
