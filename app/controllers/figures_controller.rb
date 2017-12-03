195853fca82d56b45bd6a77daddda768e47681df
miriam <miriam5000@gmail.com>
2017-12-02 20:10:34 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,13 @@
 class FiguresController < ApplicationController
 
+	# get "/index" do 
+	# 	@figures = Figure.all 
+	# 	erb :"/figures/index"
+	# end 
+
+	get "/index" do
+		"hello world" 
+		erb :"/figures/index"
+	end 
+
 end
