8ce2e54426549f6b1d59ed642ea813b2e166e1f3
miriam <miriam5000@gmail.com>
2017-12-02 21:36:34 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,11 +1,23 @@
 class FiguresController < ApplicationController
 
-	
+
+#show all figures
 	get "/index" do 
-
-		@figures = Figure.all 
-
+		@figures = Figure.all
 		erb :"/figures/index"
+	end 
+
+
+#send form for new figure
+	get "figures/new" do
+		erb :"/figures/new"
+	end 
+
+#get data from form, turn into object, send to db
+	post "/figures/new" do 
+		@figure = Figure.create(:name => params[figure[name]])
+		# ??
+		redirect to '/index'
 	end 
 
 	# get "/index" do
@@ -14,3 +26,8 @@ class FiguresController < ApplicationController
 	# end 
 
 end
+
+# @song = Song.create(:name => params["Name"])
+# 		@song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+# 		@song.genre_ids = params[:genres]
+# 		@song.save
